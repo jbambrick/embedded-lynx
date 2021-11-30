c4c4b5203d0b0637bb12ccd7485eba849b503eae
Justin Bambrick <jbambrick@tsilhqotin.ca>
2021-11-29 20:31:28 -0800

    build(embedded-lynx): add MUI React library

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,12 @@
+import * as React from 'react';
+import { ReactDOM } from 'react';
+import { Button } from '@mui/material';
+
 const Home = () => {
     return (
         <div className="home">
             <h2>Home page works!</h2>
+            <Button variant="contained">Hello World</Button>
         </div>
     );
 }
