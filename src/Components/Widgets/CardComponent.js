3dba420004ef29fd195f878405b2c724ca18c324
Justin Bambrick <jbambrick@tsilhqotin.ca>
2021-12-14 23:01:16 -0800

    style(embedded-lynx): add motion to card component

--- src/Components/Widgets/CardComponent.js
+++ src/Components/Widgets/CardComponent.js
@@ -10,18 +10,34 @@ import { motion } from "framer-motion";
 export default function CardComponent() {
     return (
 <Typography className="skillCard">
+<motion.div
+                  whileHover={{scale:1.03, }}
+                  whileTap={{ scale: 0.98 }}>
     <Card variant="outlined" style={{borderRadius: '0px'}}>
         <CardContent className="cardBlock">ReactJS</CardContent>
     </Card>
+    </motion.div>
+    <motion.div
+                  whileHover={{scale:1.03, }}
+                  whileTap={{ scale: 0.98 }}>
     <Card variant="outlined" style={{borderRadius: '0px'}}>
         <CardContent className="cardBlock">Angular</CardContent>
     </Card>
+    </motion.div>
+    <motion.div
+                  whileHover={{scale:1.03, }}
+                  whileTap={{ scale: 0.98 }}>
     <Card variant="outlined" style={{borderRadius: '0px'}}>
         <CardContent className="cardBlock">CSS/SCSS</CardContent>
     </Card>
+    </motion.div>
+    <motion.div
+                  whileHover={{scale:1.03, }}
+                  whileTap={{ scale: 0.98 }}>
     <Card variant="outlined" style={{borderRadius: '0px'}}>
         <CardContent className="cardBlock">Adobe</CardContent>
     </Card>
+    </motion.div>
 </Typography>
     
 
